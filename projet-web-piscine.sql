ba213665fc7d364445535f01fc09ce83b7db45f4
Ashtaggg <alexis1.raynal@gmail.com>
2023-05-29 12:16:57 +0200

    add html et sql

---
+++ projet-web-piscine.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 29 mai 2023 à 10:12
+-- Version du serveur : 8.0.27
+-- Version de PHP : 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `projet-web-piscine`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE IF NOT EXISTS `message` (
+  `IDmessage` int NOT NULL AUTO_INCREMENT,
+  `Envoyeur` varchar(255) NOT NULL COMMENT 'ID de l''utilisateur',
+  `Recepteur` varchar(255) NOT NULL COMMENT 'ID de l''utilisateur ou du post',
+  `Heure` int NOT NULL,
+  `Date` date NOT NULL,
+  `Contenu` text NOT NULL,
+  `Data` varchar(255) NOT NULL,
+  `Statut` int NOT NULL COMMENT 'Si le message a été vu par le recepteur',
+  PRIMARY KEY (`IDmessage`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE IF NOT EXISTS `post` (
+  `IDpost` int NOT NULL AUTO_INCREMENT,
+  `Envoyeur` varchar(255) NOT NULL COMMENT 'ID de l''utilisateur',
+  `Type` varchar(255) NOT NULL,
+  `Heure` int NOT NULL,
+  `Date` date NOT NULL,
+  `Data` varchar(255) NOT NULL,
+  `Legende` text NOT NULL,
+  `Commentaires` text NOT NULL COMMENT 'Liste des ID Messages',
+  `Like` int NOT NULL,
+  `Dislike` int NOT NULL,
+  PRIMARY KEY (`IDpost`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `IDutilisateur` int NOT NULL AUTO_INCREMENT,
+  `Type` int NOT NULL COMMENT 'Admin ou prof ou élève',
+  `Nom` varchar(255) NOT NULL,
+  `Prenom` varchar(255) NOT NULL,
+  `Age` int NOT NULL,
+  `Adresse` varchar(255) NOT NULL,
+  `PhotoProfil` varchar(255) NOT NULL,
+  `AnneeEtude` int NOT NULL,
+  `Amis` text NOT NULL,
+  `Messages` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'ID des messages',
+  `Emplois` text NOT NULL,
+  PRIMARY KEY (`IDutilisateur`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
