24cd8855334359b5d6c42bd848fd81942b445686
Ashtaggg <alexis1.raynal@gmail.com>
2023-05-29 20:33:18 +0200

    Modif bdd

--- projet-web-piscine.sql
+++ projet-web-piscine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 29 mai 2023 à 12:44
+-- Généré le : lun. 29 mai 2023 à 18:32
 -- Version du serveur : 8.0.27
 -- Version de PHP : 7.4.26
 
@@ -32,8 +32,7 @@ CREATE TABLE IF NOT EXISTS `message` (
   `IDmessage` int NOT NULL AUTO_INCREMENT,
   `Envoyeur` varchar(255) NOT NULL COMMENT 'ID de l''utilisateur',
   `Recepteur` varchar(255) NOT NULL COMMENT 'ID de l''utilisateur ou du post',
-  `Heure` int NOT NULL,
-  `Date` date NOT NULL,
+  `Date` datetime NOT NULL,
   `Contenu` text NOT NULL,
   `Data` varchar(255) NOT NULL,
   `Statut` int NOT NULL COMMENT 'Si le message a été vu par le recepteur',
@@ -51,15 +50,22 @@ CREATE TABLE IF NOT EXISTS `post` (
   `IDpost` int NOT NULL AUTO_INCREMENT,
   `Envoyeur` varchar(255) NOT NULL COMMENT 'ID de l''utilisateur',
   `Type` varchar(255) NOT NULL,
-  `Heure` int NOT NULL,
-  `Date` date NOT NULL,
+  `Date` datetime NOT NULL,
   `Data` varchar(255) NOT NULL,
   `Legende` text NOT NULL,
   `Commentaires` text NOT NULL COMMENT 'Liste des ID Messages',
   `Like` int NOT NULL,
   `Dislike` int NOT NULL,
   PRIMARY KEY (`IDpost`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `post`
+--
+
+INSERT INTO `post` (`IDpost`, `Envoyeur`, `Type`, `Date`, `Data`, `Legende`, `Commentaires`, `Like`, `Dislike`) VALUES
+(1, '1', 'photo', '2023-05-29 18:45:38', 'images/Cars.jpg', 'Je suis rapide !', '', 0, 0),
+(2, '2', 'photo', '2023-05-29 19:29:40', 'images/bob.jpg', 'Je suis une éponge !', '', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -75,6 +81,7 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `Prenom` varchar(255) NOT NULL,
   `DateNaissance` date NOT NULL,
   `Adresse` varchar(255) NOT NULL,
+  `Mail` varchar(255) NOT NULL,
   `PhotoProfil` varchar(255) NOT NULL,
   `AnneeEtude` int NOT NULL,
   `Amis` text NOT NULL,
@@ -88,9 +95,9 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
 -- Déchargement des données de la table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`IDutilisateur`, `Type`, `Nom`, `Prenom`, `DateNaissance`, `Adresse`, `PhotoProfil`, `AnneeEtude`, `Amis`, `Messages`, `Posts`, `Emplois`) VALUES
-(1, 1, 'RAYNAL', 'Alexis', '2003-10-01', 'Saint-Mandé', 'images/pp', 2, '', '', '', ''),
-(2, 1, 'GRAS', 'Mathis', '2003-06-14', 'Dans les champs', 'images/pp', 2, '', '', '', '');
+INSERT INTO `utilisateur` (`IDutilisateur`, `Type`, `Nom`, `Prenom`, `DateNaissance`, `Adresse`, `Mail`, `PhotoProfil`, `AnneeEtude`, `Amis`, `Messages`, `Posts`, `Emplois`) VALUES
+(1, 1, 'RAYNAL', 'Alexis', '2003-10-01', 'Saint-Mandé', 'alexis.raynal@edu.ece.fr', 'images/pp.jpg', 2, '', '', '1\r\n', ''),
+(2, 1, 'GRAS', 'Mathis', '2003-06-14', 'Dans les champs', 'mathis.gras@edu.ece.fr', 'images/pp.jpg', 2, '', '', '2', '');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
