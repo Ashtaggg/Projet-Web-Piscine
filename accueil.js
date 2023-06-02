d238422ed89f89477efeedd9dd4f3e807fd9b189
Ashtaggg <alexis1.raynal@gmail.com>
2023-06-02 15:39:05 +0200

    com

--- accueil.js
+++ accueil.js
@@ -60,3 +60,25 @@ function com_cacher(){
     var overlay3 = document.getElementById("overlay3");
     overlay3.style.display = "none";
 }
+
+
+function com_poster(element){
+    var IDpost = element.id;
+    var contenu = document.getElementById("contenuCom");
+    var contenu = contenu.value;
+    var Com = parseInt(element.dataset.com) + 1;
+    console.log(contenu);
+    $.ajax({
+        url: 'commentaires.php',
+        type: 'POST',
+        cache: false,
+        data: { ContenuCom: contenu, IDpostCom: IDpost, NbrCom: Com},
+        success: function(response) {
+            $('.nbrCom[data-idpost="' + IDpost + '"]').text(Com);
+            $('.php').load('commentaires.php', { IDpostCom: IDpost });
+        },
+        error: function() {
+            alert('Erreur lors de la requête AJAX');
+        }
+    });
+}
