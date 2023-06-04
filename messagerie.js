7795b6c93958a70f2d993978ea48c5382e8ad377
Mathis Gras <mathis.grs05@gmail.com>
2023-06-04 03:34:40 +0200

    search msg

--- messagerie.js
+++ messagerie.js
@@ -1,5 +1,24 @@
+function rech_mess(element){
+    var input = $('#rech-message');
+    var contenu = input.val();
+    $.ajax({
+        url: 'rech_messages.php',
+        type: 'POST',
+        cache: false,
+        //data: { IDenv: envoyeur, IDrecep: recepteur, Contenu: contenu},
+        success: function(response) {
+            input.val('');
+            $('#scroll-messagerie').load('rech_messages.php', { Contenu: contenu});
+        },
+        error: function() {
+            alert('Erreur lors de la requête AJAX');
+        }
+    });
+}
+
+
 function mess(element){
-    var input = $('#contenuMess'); // Sélectionnez l'élément input par son ID
+    var input = $('#contenuMess');
     var contenu = input.val();
     var envoyeur = parseInt(element.dataset.env);
     var recepteur = parseInt(element.dataset.recep);
